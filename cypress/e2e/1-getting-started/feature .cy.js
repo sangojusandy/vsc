4c1ea9cb5c366f0bd506a96eb74930d183f2c046
sandysangoju <sandeepdeep270@gmail.com>
2024-05-11 23:37:59 +0530

    login and search

--- cypress/e2e/1-getting-started/feature .cy.js
+++ cypress/e2e/1-getting-started/feature .cy.js
@@ -1,3 +1,12 @@
 //Launch the site
-cy.visit();
-cy.wait();
+describe('Flipkart validation', function() {
+    //Launch the app and search for mobile
+    it('should launch the app', function() {
+        cy.visit("https://www.flipkart.com/");
+        cy.contains('Mobiles').click();
+        cy.get('[name="q"]').type('samsung');
+        cy.get('[type="submit"]').click();
+    });
+
+    
+  });
